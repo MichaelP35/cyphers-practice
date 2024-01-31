9c5b524a0790a6322451a144f9383a1bafca7768
MichaelP35 <michael.poplawski35@gmail.com>
2024-01-31 13:56:14 -0600

    implemented vigenere and updated caesar

--- main.py
+++ main.py
@@ -5,17 +5,51 @@ class Ciphers:
 
     # Caesar Cipher
     def caesar(self, text: str, offset:int):
+        charUpper = 0
         new_text = ''
 
-        # Make text all lowercase. Temporary workaround.
-        for char in text.lower():
-            # Append any non-letter character to the message.
+        # Go through each character in text
+        for char in text:
+            # Append any non-letter character to the message
             if not char.isalpha():
                 new_text += char
-            # If letter, shift letter by inputted offset.
+            elif char.isupper():
+                char = char.lower()
+                index = self.alphabet.find(char)
+                new_index = (index + offset) % len(self.alphabet)
+                new_text += self.alphabet[new_index].upper()
+            # If lower case letter, shift letter by inputted offset
             else:
                 index = self.alphabet.find(char)
                 new_index = (index + offset) % len(self.alphabet)
+                new_text += self.alphabet[new_index]
+                
+        
+        return new_text
+    
+
+    # Vigenere Cipher
+    def vigenere(self, text: str, key:str, direction: int=1):
+        key_index = 0
+        new_text = ''
+
+        for char in text:
+            if not char.isalpha():
+                new_text += char
+            elif char.isupper():
+                char = char.lower()
+                key_char = key[key_index % len(key)]
+                key_index +=1
+                offset = self.alphabet.index(key_char)
+                index = self.alphabet.find(char)
+                new_index = ((index + offset*direction) % len(self.alphabet))
+                new_text += self.alphabet[new_index].upper()
+            else:
+                key_char = key[key_index % len(key)]
+                key_index +=1
+                offset = self.alphabet.index(key_char)
+                index = self.alphabet.find(char)
+                new_index = ((index + offset*direction) % len(self.alphabet))
                 new_text += self.alphabet[new_index]
         
         return new_text
